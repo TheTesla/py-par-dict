776269d79a9ce4e44bfa065c211d25847b778057
Stefan Helmert <stefan.helmert@t-online.de>
2023-11-26 16:53:04 +0100

    parDict works now! ToDo: FiFo upper limit, native interfacing

--- testthreadid.py
+++ testthreadid.py
@@ -15,20 +15,23 @@ def par(x):
     fifos_k = np.zeros((n,n,fifo_cap),dtype=np.int64)
     fifos_v = np.zeros((n,n,fifo_cap),dtype=np.float64)
     fifo_idx = np.zeros((n,n,2),dtype=np.int64)
-    d = [{0:0.}] * n
-    y = [0.]*len(x)
+
+    # This variant crashes:
+    # d = [{0:0.}] * n
+
+    # This variant works:
+    d = [{0:0.} for i in range(n)]
+
 
 
     for i in prange(len(x)):
         # just create some data
-        y[i] = (x[i]**1.2 + i**1.4)**0.9
-
-
+        y = (x[i]**1.2 + i**1.4)**0.9
 
 
         # Arguments of parDict.setitem(k, v)
         k = i
-        v = y[i]
+        v = y
 
         # split and fill fifos
         src_id = nb.get_thread_id()
@@ -42,21 +45,17 @@ def par(x):
             dst_id = nb.get_thread_id()
             if fifo_idx[src_id,dst_id,1] == fifo_idx[src_id,dst_id,0]:
                 continue
-            if (fifo_idx[src_id,dst_id,1]+1)%fifo_cap == fifo_idx[src_id,dst_id,0]:
-                continue
-            if (fifo_idx[src_id,dst_id,1]+2)%fifo_cap == fifo_idx[src_id,dst_id,0]:
-                continue
             k2 = fifos_k[src_id,dst_id,fifo_idx[src_id,dst_id,1]]
             v2 = fifos_v[src_id,dst_id,fifo_idx[src_id,dst_id,1]]
             fifo_idx[src_id,dst_id,1] = (fifo_idx[src_id,dst_id,1] + 1) % \
             fifo_cap
             d[dst_id][k2] = v2
 
-
-
     return d
 
-x = np.random.rand(1000)
+nb.set_num_threads(16)
+
+x = np.random.rand(100000000)
 print(x[0])
 
 print(par(x)[0])
