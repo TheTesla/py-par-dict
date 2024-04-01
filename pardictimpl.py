4743906ba97879f43bc419daba22b6c53bdab922
Stefan Helmert <stefan.helmert@t-online.de>
2024-04-01 18:16:51 +0200

    sync works

--- pardictimpl.py
+++ pardictimpl.py
@@ -3,7 +3,7 @@ import numpy as np
 import numba as nb
 from numba import njit, prange
 
-@njit
+@njit(parallel=True)
 def new_par_dict(key_type, val_type, nothrds=4, fifo_size=1024):
     keys = np.zeros((nothrds,nothrds,fifo_size),dtype=key_type)
     vals = np.zeros((nothrds,nothrds,fifo_size),dtype=val_type)
@@ -13,16 +13,21 @@ def new_par_dict(key_type, val_type, nothrds=4, fifo_size=1024):
             for i in range(nothrds)]
     return (keys,vals,rd_idx,wr_idx,fifo_size,nothrds,dicts)
 
-@njit
 def par_dict_setitem(state, key, val, thrd_id=None):
     if thrd_id is None:
         thrd_id = nb.get_thread_id()
     thrd_id = int(thrd_id)
 
-    # write into FIFO
     keys, vals, rd_idx, wr_idx, fifo_size, nothrds, dicts = state
+
+
+    # write into FIFO
     dst_id = hash(key)%nothrds
     src_id = thrd_id
+    # check FIFO overflow
+    cap = (rd_idx[src_id, dst_id] - wr_idx[src_id, dst_id] - 1)%fifo_size
+    if cap == 0:
+        print("FIFO overflow!")
     wridx = wr_idx[src_id, dst_id]
     keys[src_id, dst_id, wridx] = key
     vals[src_id, dst_id, wridx] = val
@@ -31,29 +36,45 @@ def par_dict_setitem(state, key, val, thrd_id=None):
     # read FIFO into dicts
     dst_id = thrd_id
     for src_id in range(nothrds):
-        if wr_idx[src_id, dst_id] == rd_idx[src_id, dst_id]:
-            continue
-        k = keys[src_id, dst_id, rd_idx[src_id, dst_id]]
-        v = vals[src_id, dst_id, rd_idx[src_id, dst_id]]
-        rd_idx[src_id, dst_id] = (rd_idx[src_id, dst_id] + 1)%fifo_size
-        dicts[dst_id][k] = v
+        while wr_idx[src_id, dst_id] != rd_idx[src_id, dst_id]:
+            k = keys[src_id, dst_id, rd_idx[src_id, dst_id]]
+            v = vals[src_id, dst_id, rd_idx[src_id, dst_id]]
+            rd_idx[src_id, dst_id] = (rd_idx[src_id, dst_id] + 1)%fifo_size
+            dicts[dst_id][k] = v
 
+@njit(parallel=True)
+def par_dict_sync(state):
+    keys, vals, rd_idx, wr_idx, fifo_size, nothrds, dicts = state
+    for dst_id in prange(nothrds):
+        for src_id in range(nothrds):
+            while wr_idx[src_id, dst_id] != rd_idx[src_id, dst_id]:
+                k = keys[src_id, dst_id, rd_idx[src_id, dst_id]]
+                v = vals[src_id, dst_id, rd_idx[src_id, dst_id]]
+                rd_idx[src_id, dst_id] = (rd_idx[src_id, dst_id] + 1)%fifo_size
+                dicts[dst_id][k] = v
+
+
+@njit
 def par_dict_getitem(state, key, val):
     keys, vals, rd_idx, wr_idx, fifo_size, nothrds, dicts = state
     return dicts[hash(key)%nothrds][key]
 
-@njit
+@njit(parallel=True)
 def demo():
-    n = 1000
+    n = 100000000
     no_threads = nb.get_num_threads()
-    pdict = new_par_dict(np.int64, nb.types.float64, no_threads, 1024)
+    pdict = new_par_dict(np.int64, nb.types.float64, no_threads, 10240000)
     par_dict_setitem(pdict, int(23), 42.0)
 
     for i in prange(n):
         k = i
-        v = i*3.0
+        v = np.sin(i*3.0)
         par_dict_setitem(pdict, k, v)
-    print(pdict)
+
+    print([len(e) for e in pdict[-1]])
+    par_dict_sync(pdict)
+    print([len(e) for e in pdict[-1]])
+    #print(pdict[-1])
 
 
 
