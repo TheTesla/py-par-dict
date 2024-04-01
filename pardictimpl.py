053b43437d2b725fb948770493bde7bbc2505aac
Stefan Helmert <stefan.helmert@t-online.de>
2024-04-01 16:22:31 +0200

    class like implementation started

---
+++ pardictimpl.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+import numpy as np
+import numba as nb
+from numba import njit, prange
+
+@njit
+def new_par_dict(key_type, val_type, nothrds=4, fifo_size=1024):
+    keys = np.zeros((nothrds,nothrds,fifo_size),dtype=key_type)
+    vals = np.zeros((nothrds,nothrds,fifo_size),dtype=val_type)
+    rd_idx = np.zeros((nothrds,nothrds),dtype=np.int64)
+    wr_idx = np.zeros((nothrds,nothrds),dtype=np.int64)
+    dicts = [nb.typed.Dict.empty(key_type=key_type, value_type=val_type) \
+            for i in range(nothrds)]
+    return (keys,vals,rd_idx,wr_idx,fifo_size,nothrds,dicts)
+
+@njit
+def par_dict_setitem(state, key, val, thrd_id=None):
+    if thrd_id is None:
+        thrd_id = nb.get_thread_id()
+    thrd_id = int(thrd_id)
+
+    # write into FIFO
+    keys, vals, rd_idx, wr_idx, fifo_size, nothrds, dicts = state
+    dst_id = hash(key)%nothrds
+    src_id = thrd_id
+    wridx = wr_idx[src_id, dst_id]
+    keys[src_id, dst_id, wridx] = key
+    vals[src_id, dst_id, wridx] = val
+    wr_idx[src_id, dst_id] = (wr_idx[src_id, dst_id] + 1)%fifo_size
+
+    # read FIFO into dicts
+    dst_id = thrd_id
+    for src_id in range(nothrds):
+        if wr_idx[src_id, dst_id] == rd_idx[src_id, dst_id]:
+            continue
+        k = keys[src_id, dst_id, rd_idx[src_id, dst_id]]
+        v = vals[src_id, dst_id, rd_idx[src_id, dst_id]]
+        rd_idx[src_id, dst_id] = (rd_idx[src_id, dst_id] + 1)%fifo_size
+        dicts[dst_id][k] = v
+
+def par_dict_getitem(state, key, val):
+    keys, vals, rd_idx, wr_idx, fifo_size, nothrds, dicts = state
+    return dicts[hash(key)%nothrds][key]
+
+@njit
+def demo():
+    n = 1000
+    no_threads = nb.get_num_threads()
+    pdict = new_par_dict(np.int64, nb.types.float64, no_threads, 1024)
+    par_dict_setitem(pdict, int(23), 42.0)
+
+    for i in prange(n):
+        k = i
+        v = i*3.0
+        par_dict_setitem(pdict, k, v)
+    print(pdict)
+
+
+
+
+if __name__ == "__main__":
+    demo()
+
+
