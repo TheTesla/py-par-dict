624c833e460f5c86f9f9c1980c8aa243c92f6d7c
Stefan Helmert <stefan.helmert@htw-dresden.de>
2024-04-03 10:57:22 +0200

    speedup now visible in virtualbox

--- pardictimpl.py
+++ pardictimpl.py
@@ -4,6 +4,7 @@ import numba as nb
 from numba import njit, prange
 import time
 
+#@njit(parallel=True,nogil=True)
 @njit(parallel=True)
 def new_par_dict(key_type, val_type, nothrds=4, fifo_size=1024):
     keys = np.zeros((nothrds,nothrds,fifo_size),dtype=key_type)
@@ -12,9 +13,10 @@ def new_par_dict(key_type, val_type, nothrds=4, fifo_size=1024):
     rd_idx = np.zeros((nothrds,nothrds),dtype=np.int64)
     wr_idx = np.zeros((nothrds,nothrds),dtype=np.int64)
     dicts = [nb.typed.Dict.empty(key_type=key_type, value_type=val_type) \
-            for i in range(nothrds)]
+							for e in range(nothrds)]
     return (keys,vals,cmds,rd_idx,wr_idx,fifo_size,nothrds,dicts)
 
+#@njit(nogil=True)
 @njit
 def par_dict_setitem(state, key, val, thrd_id=None):
     if thrd_id is None:
@@ -39,17 +41,19 @@ def par_dict_setitem(state, key, val, thrd_id=None):
 
     # read FIFO into dicts
     dst_id = thrd_id
-    for src_id in range(nothrds):
-        while wr_idx[src_id, dst_id] != rd_idx[src_id, dst_id]:
-            k = keys[src_id, dst_id, rd_idx[src_id, dst_id]]
-            v = vals[src_id, dst_id, rd_idx[src_id, dst_id]]
-            c = cmds[src_id, dst_id, rd_idx[src_id, dst_id]]
-            rd_idx[src_id, dst_id] = (rd_idx[src_id, dst_id] + 1)%fifo_size
-            if c:
-                dicts[dst_id][k] = v
-            else:
-                del dicts[dst_id][k]
+    if hash(key)%1000 == 0:
+        for src_id in range(nothrds):
+            while wr_idx[src_id, dst_id] != rd_idx[src_id, dst_id]:
+                k = keys[src_id, dst_id, rd_idx[src_id, dst_id]]
+                v = vals[src_id, dst_id, rd_idx[src_id, dst_id]]
+                c = cmds[src_id, dst_id, rd_idx[src_id, dst_id]]
+                rd_idx[src_id, dst_id] = (rd_idx[src_id, dst_id] + 1)%fifo_size
+                if c:
+                    dicts[dst_id][k] = v
+                else:
+                    del dicts[dst_id][k]
 
+#@njit(nogil=True)
 @njit
 def par_dict_delitem(state, key, thrd_id=None):
     if thrd_id is None:
@@ -73,17 +77,19 @@ def par_dict_delitem(state, key, thrd_id=None):
 
     # read FIFO into dicts
     dst_id = thrd_id
-    for src_id in range(nothrds):
-        while wr_idx[src_id, dst_id] != rd_idx[src_id, dst_id]:
-            k = keys[src_id, dst_id, rd_idx[src_id, dst_id]]
-            v = vals[src_id, dst_id, rd_idx[src_id, dst_id]]
-            c = cmds[src_id, dst_id, rd_idx[src_id, dst_id]]
-            rd_idx[src_id, dst_id] = (rd_idx[src_id, dst_id] + 1)%fifo_size
-            if c:
-                dicts[dst_id][k] = v
-            else:
-                del dicts[dst_id][k]
+    if hash(key)%1000 == 0:
+        for src_id in range(nothrds):
+            while wr_idx[src_id, dst_id] != rd_idx[src_id, dst_id]:
+                k = keys[src_id, dst_id, rd_idx[src_id, dst_id]]
+                v = vals[src_id, dst_id, rd_idx[src_id, dst_id]]
+                c = cmds[src_id, dst_id, rd_idx[src_id, dst_id]]
+                rd_idx[src_id, dst_id] = (rd_idx[src_id, dst_id] + 1)%fifo_size
+                if c:
+                    dicts[dst_id][k] = v
+                else:
+                    del dicts[dst_id][k]
 
+#@njit(parallel=True,nogil=True)
 @njit(parallel=True)
 def par_dict_sync(state):
     keys, vals, cmds, rd_idx, wr_idx, fifo_size, nothrds, dicts = state
@@ -100,22 +106,24 @@ def par_dict_sync(state):
                     del dicts[dst_id][k]
 
 
+#@njit(nogil=True)
 @njit
 def par_dict_getitem(state, key):
     keys, vals, cmds, rd_idx, wr_idx, fifo_size, nothrds, dicts = state
     return dicts[hash(key)%nothrds][key]
 
+#@njit(parallel=True,nogil=True)
 @njit(parallel=True)
 def demo():
-    n = 10000000
+    n = 8000000
     no_threads = nb.get_num_threads()
-    #no_threads = 16 
+    #no_threads = 8
     pdict = new_par_dict(np.int64, nb.types.float64, no_threads, 2**20)
     par_dict_setitem(pdict, int(23), 42.0)
 
     for i in prange(n):
         k = i
-        v = np.sin(8**(1/i)*3.0)**0.3
+        v = np.sin(8**(1/(1+i))*3.0)**0.3
         par_dict_setitem(pdict, k, v)
 
     print([len(e) for e in pdict[-1]])
